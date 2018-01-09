5cb1576617c6e81d8389bcfcb95a7365cc7ab680
Coviam <coviam@Viral-Shahs-MacBook-Pro.local>
2018-01-09 20:28:34 +0530

    Final Commit 1

--- EmployeeManagement/src/model/JsonFileHandler.java
+++ EmployeeManagement/src/model/JsonFileHandler.java
@@ -1,18 +1,90 @@
 package model;
 
-public class JsonFileHandler implements MyFileHandler{
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSyntaxException;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+
+public class JsonFileHandler implements MyFileHandler{
+	 JsonReader reader ;
+	 //JsonWriter writer;
+	JsonFileHandler()
+	{
+		   try {
+		    	FileInputStream geek = new FileInputStream("/Users/coviam/eclipse-workspace/EmployeeManagement/src/model/employee.json");
+		//     	FileOutputStream geek1= new FileOutputStream("/Users/coviam/eclipse-workspace/EmployeeManagement/src/model/employee.json");
+		//		    	writer= new JsonWriter(new OutputStreamWriter(geek1));
+		         reader = new JsonReader(new InputStreamReader(geek));
+		        Gson gson = new GsonBuilder()
+		 			   .setDateFormat("MM/dd/yy").create();
+//writer.beginArray();
+		        // Read file in stream mode
+		        reader.beginArray();
+		      
+		   } catch (UnsupportedEncodingException ex) {
+		       
+		    } catch (IOException ex) {
+		       
+		    }
+
+	}
 	@Override
 	public Employee read() {
 		// TODO Auto-generated method stub
-		
+		Gson gson = new GsonBuilder()
+	 			   .setDateFormat("MM/dd/yy").create();
+
+		  try {
+			while (reader.hasNext()) {
+			        // Read data into object model
+			        Employee employee = gson.fromJson(reader, Employee.class);
+			        return employee;
+			   
+			    }
+		} catch (JsonIOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonSyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	        
+	   
 		return null;
 	}
 
 	@Override
 	public void write(Employee data) {
 		// TODO Auto-generated method stub
+		try {
+			FileWriter f1=new FileWriter("/Users/coviam/eclipse-workspace/EmployeeManagement/src/model/employee1.json",true);
+			JsonObject j1=new JsonObject();
+			j1.addProperty("firstName",data.getFirstName());
+			j1.addProperty("lastName", data.getLastName());
+			//j1.addProperty("dateOfBirth", data.getDateOfBirth().toString());
+			//j1.addProperty("experience", data.getExperience());
+			f1.write(j1.toString());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
+			    
 	}
 
 }
--- EmployeeManagement/src/model/temp.java
+++ EmployeeManagement/src/model/temp.java
@@ -1,31 +1,56 @@
 package model;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import org.codehaus.jackson.JsonFactory;
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.JsonParser;
+import java.io.*;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
 
 public class temp {
-public static void main(String[] args) {
-	Gson gson = new GsonBuilder()
-			   .setDateFormat("MM/dd/yy").create();
-	JsonReader reader;
-	try {
-		reader = new JsonReader(new FileReader("/Users/coviam/eclipse-workspace/EmployeeManagement/src/model/employee.json"));
-		Type myDataType = new TypeToken<Collection<Employee>>(){}.getType();
-		
-		Employee data= ((ArrayList<Employee>)gson.fromJson(reader, myDataType)).get(0);
-		
+	
+	public static void readStream() {
+	    try {
+	    	FileInputStream geek = new FileInputStream("/Users/coviam/eclipse-workspace/EmployeeManagement/src/model/employee.json");
+	    	 
+	        JsonReader reader = new JsonReader(new InputStreamReader(geek));
+	        Gson gson = new GsonBuilder()
+	 			   .setDateFormat("MM/dd/yy").create();
 
-	} catch (FileNotFoundException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+	        // Read file in stream mode
+	        reader.beginArray();
+	        while (reader.hasNext()) {
+	            // Read data into object model
+	            Employee person = gson.fromJson(reader, Employee.class);
+	            System.out.println(person.firstName);
+	       break;
+	        }
+	        reader.close();
+	    } catch (UnsupportedEncodingException ex) {
+	       
+	    } catch (IOException ex) {
+	       
+	    }
 	}
+public static void main(String[] args) {
+JsonFileHandler j=new JsonFileHandler();
+for(int i=0;i<10;i++)
+{
+j.read();	
+}
+Employee e=new Employee();
+e.setFirstName("viral");
+e.setLastName("shah");
+j.write(e);
 }
 }
